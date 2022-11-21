79edc04516df9a984d02a14b104aceaa619aa99d
harshglobal <harsh.gawande@gmail.com>
2022-11-21 20:25:22 +0530

    FIle-2

--- edelweiss/settings.py
+++ edelweiss/settings.py
@@ -118,9 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # STATIC_ROOT = '/root/NseData/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_URL = '/static/'
 
+STATIC_URL = 'static/'
+STATICFILES_DIR = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

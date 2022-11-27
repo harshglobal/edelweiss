5114a762d7457fec5cbf88fc922b9a21ab241355
harshglobal <harsh.gawande@gmail.com>
2022-11-27 16:06:18 +0530

    Trading view data added

--- edelweiss/settings.py
+++ edelweiss/settings.py
@@ -119,12 +119,14 @@ USE_TZ = True
 
 # STATIC_ROOT = '/root/NseData/static/'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = '/static/'
     
-STATIC_ROOT = BASE_DIR / 'static'
+if not DEBUG:
+    STATIC_ROOT = ''
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
